48348f1e41e4b84310f9a90d9b3a1c1bd43d4416
wmxiong <wmxiong@pku.edu.cn>
2024-07-17 08:41:07 +0000

    update

--- eval_agent/envs/__init__.py
+++ eval_agent/envs/__init__.py
@@ -1,5 +1,4 @@
 from .base import BaseEnv
 from .webshop_env import WebShopEnv
-from .sciworld_env import SciWorldEnv
 from .alfworld_env import AlfWorldEnv, BatchAlfWorldEnv
 from .intercode_sql_env import IntercodeSQLEnv
